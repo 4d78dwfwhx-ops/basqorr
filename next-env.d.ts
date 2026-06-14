a8d9bd95baef3d811c6873ef507ec8bc064c1eaf
4d78dwfwhx-ops <4d78dwfwhx@privaterelay.appleid.com>
2026-06-14 10:22:25 +0300

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,7 @@
+cat > next-env.d.ts << 'EOF'
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+EOF
