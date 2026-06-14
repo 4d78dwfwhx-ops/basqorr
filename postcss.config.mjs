545444bcef72936b4aed30ebdcb04221a1fe4833
4d78dwfwhx-ops <4d78dwfwhx@privaterelay.appleid.com>
2026-06-14 10:23:20 +0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,11 @@
+cat > postcss.config.mjs << 'EOF'
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
 export default config;
+EOF
